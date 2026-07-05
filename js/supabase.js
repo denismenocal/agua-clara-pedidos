a66f29a1d274283f54c37b725c34265400366f72
denismenocal <130603937+denismenocal@users.noreply.github.com>
2026-07-05 13:46:03 -0600

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://ayzddkcxeswjfpinwvth.supabase.co";
+
+const SUPABASE_KEY =
+"TU_PUBLIC_KEY";
+
+const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
